d968d5ab57affa8d994ccfbb6e9e69964cf74b1f
matias larroque <larroquematias@gmail.com>
2019-09-18 11:51:39 -0300

    se hace proyecto para uart y SPI, se añade uart.h y uart.c

---
+++ guiaUartSPI/proyectsUartSPI/uartSPI/source/uart.c
@@ -0,0 +1,38 @@
+/*
+ * uart.c
+ *
+ *  Created on: 18 sep. 2019
+ *      Author: G5
+ */
+
+#include "uart.h"
+
+void uartInit (uint8_t id, uart_cfg_t config)
+{
+
+}
+
+uint8_t uartIsRxMsg(uint8_t id)
+{
+
+}
+
+uint8_t uartGetRxMsgLength(uint8_t id)
+{
+
+}
+
+uint8_t uartReadMsg(uint8_t id, char* msg, uint8_t cant)
+{
+
+}
+
+uint8_t uartWriteMsg(uint8_t id, const char* msg, uint8_t cant)
+{
+
+}
+
+uint8_t uartIsTxMsgComplete(uint8_t id)
+{
+
+}
