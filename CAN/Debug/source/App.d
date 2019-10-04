9639d66f7bf04f3e9fc56bd91d6c02dd5e088a5d
Manuel Mollon <mmollon@itba.edu.ar>
2019-10-04 20:26:43 -0300

    funciona CANNNNNN

---
+++ CAN/Debug/source/App.d
@@ -0,0 +1,3 @@
+source/App.o source/App.d: ../source/App.c ../source/can.h
+
+../source/can.h:
---
+++ CAN/Debug/source/can.d
@@ -0,0 +1,33 @@
+source/can.o source/can.d: ../source/can.c ../source/can.h \
+ ../SDK/CMSIS/MK64F12.h ../SDK/CMSIS/core_cm4.h \
+ ../SDK/CMSIS/cmsis_version.h ../SDK/CMSIS/cmsis_compiler.h \
+ ../SDK/CMSIS/cmsis_gcc.h ../SDK/CMSIS/system_MK64F12.h \
+ ../SDK/startup/hardware.h ../SDK/CMSIS/fsl_device_registers.h \
+ ../SDK/CMSIS/MK64F12.h ../SDK/CMSIS/MK64F12_features.h \
+ ../SDK/CMSIS/core_cm4.h ../source/pinsHandler.h
+
+../source/can.h:
+
+../SDK/CMSIS/MK64F12.h:
+
+../SDK/CMSIS/core_cm4.h:
+
+../SDK/CMSIS/cmsis_version.h:
+
+../SDK/CMSIS/cmsis_compiler.h:
+
+../SDK/CMSIS/cmsis_gcc.h:
+
+../SDK/CMSIS/system_MK64F12.h:
+
+../SDK/startup/hardware.h:
+
+../SDK/CMSIS/fsl_device_registers.h:
+
+../SDK/CMSIS/MK64F12.h:
+
+../SDK/CMSIS/MK64F12_features.h:
+
+../SDK/CMSIS/core_cm4.h:
+
+../source/pinsHandler.h:
