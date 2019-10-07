64886f95e99a3ba035ee9d792d22224e10eecf58
LuFernandez <lu.gfernandez@gmail.com>
2019-10-06 23:34:37 -0300

    actualizada orientacion de magnetometro

--- guiaUartSPI/proyectsUartSPI/uartSPI/source/accelerometer.h
+++ guiaUartSPI/proyectsUartSPI/uartSPI/source/accelerometer.h
@@ -8,10 +8,24 @@
 #ifndef ACCELEROMETER_H_
 #define ACCELEROMETER_H_
 
+
 #include <stdint.h>
 
 
+/**
+ * @brief Initialize Accelerometer and Magnetometer, with the corresponding default protocol (IC2_0)
+ * @param none
+ * @return none
+*/
 void accelandMagnetInit(void);
+
+/**
+ * @brief Returns Accelerometer and Magnetometer info: roll angle, head angle, orientation.
+ * @param roll angle of device.
+ * @param head/pitch angle of device.
+ * @param orientation of device.
+ * @return none
+*/
 void getAccelAndMagntData(int16_t* roll, int16_t* head, int16_t* orientation);
 
 #endif /* ACCELEROMETER_H_ */
